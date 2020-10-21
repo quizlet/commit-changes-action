d2cc7aefb779c85c26a97fde7652aa6d7b9bbc33
cooperbenson-qz <cooper.benson@quizlet.com>
2020-10-21 10:05:36 -0600

    Getting linting working

--- src/wait.ts
+++ src/wait.ts
@@ -1,9 +1,10 @@
 export async function wait(milliseconds: number): Promise<string> {
   return new Promise(resolve => {
+    // eslint-disable-next-line no-restricted-globals
     if (isNaN(milliseconds)) {
-      throw new Error('milliseconds not a number')
+      throw new Error('milliseconds not a number');
     }
 
-    setTimeout(() => resolve('done!'), milliseconds)
-  })
+    setTimeout(() => resolve('done!'), milliseconds);
+  });
 }
